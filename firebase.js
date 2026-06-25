a6c4ce2f83cf4621f099581dc9f82a588b7a6f79
ShoaibRaqimOfficial <westanking2@gmail.com>
2026-06-25 19:03:51 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8O5zS5-nxLm2EqL6yf9ANw7H9UF0dPJE",
+  authDomain: "sro-academy-7454d.firebaseapp.com",
+  projectId: "sro-academy-7454d",
+  storageBucket: "sro-academy-7454d.firebasestorage.app",
+  messagingSenderId: "959907656036",
+  appId: "1:959907656036:web:fddd1f007120ae60d38a71",
+  measurementId: "G-YPPNNBR494"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+};
