aa39cc43c5fd3d7592a6bea51fb827409593ca8e
ShoaibRaqimOfficial <westanking2@gmail.com>
2026-06-25 19:54:10 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,46 @@
+import {
+    db
+} from "./firebase.js";
 
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const form = document.getElementById("assignmentForm");
+
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const dueDate = document.getElementById("dueDate");
+const download = document.getElementById("download");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+        await addDoc(collection(db, "assignments"), {
+
+            title: title.value,
+            description: description.value,
+            dueDate: dueDate.value,
+            download: download.value,
+            status: "Active"
+
+        });
+
+        message.style.color = "green";
+        message.textContent = "Assignment Published Successfully!";
+
+        form.reset();
+
+    } catch (error) {
+
+        message.style.color = "red";
+        message.textContent = error.message;
+
+    }
+
+});
