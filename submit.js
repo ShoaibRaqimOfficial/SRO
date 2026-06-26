36429bd0d930847f7f53b403c3bc5914ddce7ba9
ShoaibRaqimOfficial <westanking2@gmail.com>
2026-06-26 08:18:05 -0400

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,34 +1,62 @@
-import { db } from "./firebase.js";
+import {
+    db,
+    auth,
+    onAuthStateChanged
+} from "./firebase.js";
 
 import {
-  collection,
-  addDoc
+    collection,
+    addDoc
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const form = document.getElementById("submitForm");
-
-const studentName = document.getElementById("studentName");
-const studentEmail = document.getElementById("studentEmail");
 const assignmentLink = document.getElementById("assignmentLink");
 const message = document.getElementById("message");
 
-// URL se Assignment ID lena
+// URL se Assignment ID
 const params = new URLSearchParams(window.location.search);
 const assignmentId = params.get("id");
+
+// Login Check
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.href = "login.html";
+
+    }
+
+});
+
+// Submit
 
 form.addEventListener("submit", async (e) => {
 
     e.preventDefault();
 
+    const user = auth.currentUser;
+
+    if (!user) {
+
+        message.style.color = "red";
+        message.textContent = "Please login first.";
+
+        return;
+
+    }
+
     try {
+
+        // Email se temporary name banana
+        const studentName = user.email.split("@")[0];
 
         await addDoc(collection(db, "submissions"), {
 
             assignmentId: assignmentId,
 
-            studentName: studentName.value,
+            studentName: studentName,
 
-            studentEmail: studentEmail.value,
+            studentEmail: user.email,
 
             assignmentLink: assignmentLink.value,
 
@@ -39,7 +67,7 @@ form.addEventListener("submit", async (e) => {
         });
 
         message.style.color = "green";
-        message.textContent = "Assignment Submitted Successfully!";
+        message.textContent = "✅ Assignment Submitted Successfully!";
 
         form.reset();
 
