dc0d9ab410e3e80ba1eeb60c50192a9d01fc5437
ShoaibRaqimOfficial <westanking2@gmail.com>
2026-06-27 02:21:59 -0400

    Update register.js

--- register.js
+++ register.js
@@ -1,1 +1,82 @@
+import {
+    auth,
+    db,
+    createUserWithEmailAndPassword,
+    doc,
+    setDoc
+} from "./firebase.js";
 
+const registerForm = document.getElementById("registerForm");
+
+const fullName = document.getElementById("fullName");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const dob = document.getElementById("dob");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const message = document.getElementById("message");
+
+registerForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    if (password.value !== confirmPassword.value) {
+
+        message.style.color = "red";
+        message.textContent = "Passwords do not match.";
+
+        return;
+
+    }
+
+    try {
+
+        const userCredential = await createUserWithEmailAndPassword(
+
+            auth,
+            email.value,
+            password.value
+
+        );
+
+        const user = userCredential.user;
+
+        await setDoc(doc(db, "students", user.uid), {
+
+            uid: user.uid,
+
+            fullName: fullName.value,
+
+            email: email.value,
+
+            phone: phone.value,
+
+            dob: dob.value,
+
+            profileImage: "",
+
+            status: "Active",
+
+            joinedAt: new Date().toISOString()
+
+        });
+
+        message.style.color = "green";
+        message.textContent = "Account Created Successfully! Redirecting...";
+
+        setTimeout(() => {
+
+            window.location.href = "login.html";
+
+        }, 1500);
+
+    }
+
+    catch (error) {
+
+        message.style.color = "red";
+        message.textContent = error.message;
+
+    }
+
+});
